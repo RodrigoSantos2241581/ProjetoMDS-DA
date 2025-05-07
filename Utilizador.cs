7e1fd5a79cd7d80912f0a53bc96599956dd07579
Rodrigo Carvalho Santos <2241581@my.ipleiria.pt>
2025-05-07 11:55:45 +0100

    Comecar

---
+++ Utilizador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks
+{
+    class Utilizador
+    {
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string TipoUtilizador { get; set; }
+    }
+}
---
+++ iTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks
+{
+    class iTask : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
