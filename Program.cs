6fc9551d55948558c997b906d48f2633fb44bca0
JoaoDMCarreira <2241592@my.ipleiria.pt>
2025-05-14 13:01:18 +0100

    ser~s wir fdda»?

--- Program.cs
+++ Program.cs
@@ -3,34 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace iTasks
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (var db = new Utilizadores())
+            using (var db = new iTask())
             {
-                var cliente = new Cliente { Nome = "Cliente1", Nif = "123456789" };
-                db.Clientes.Add(cliente);
-                var carro = new Carro { Matricula = "AB-01-AA", Cliente = cliente };
-                db.Carro.Add(carro);
-                var servico = new Servico { Tipo = "Revisão", Data = DateTime.Now, Carro = carro };
-                db.Servicos.Add(servico);
-                var servico2 = new Servico { Tipo = "Revisão 2", Data = DateTime.Now, Carro = carro };
-                db.Servicos.Add(servico);
-                var parcela = new Parcela { Valor = 100, Descricao = "Ta bom", Servico = servico };
-                db.Parcelas.Add(parcela);
-                var parcela2 = new Parcela { Valor = 200, Descricao = "Ta bom 2", Servico = servico };
-                db.Parcelas.Add(parcela);
-                db.SaveChanges();
+                if (!db.Utilizadores.Any(u => u.Username == "Gestor"))
+                {
+                    var gestor = new Gestor
+                    {
+                        Nome = "Gestor",
+                        Username = "Gestor",
+                        Password = "1234",
+                        TipoUtilizador = "Gestor",
+                        Departamento = "IT",
+                        GereUtilizadores = "Sim"
+                    };
+
+                    db.Utilizadores.Add(gestor);
+                    db.SaveChanges();
+                }
             }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
         }
     }
 }
--- Utilizador.cs
+++ Utilizador.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace iTasks
 {
-    class Utilizador
+    public class Utilizador
     {
+        public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string TipoUtilizador { get; set; }
     }
 }
